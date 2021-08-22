fb2ece1ce30dd6394a0dc73b5f03c41e8f5d35d0
97PHPDeveloper <am.asky97@gmail.com>
2021-08-22 20:44:32 +0530

    php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculate of Two numbers</title>
 </head>
 
@@ -11,6 +12,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100&display=swap" rel="stylesheet">
+
+    <style>
+
+    @media (max-width :480px) 
+    {
+        .container{width:100%;}
+        .button{width:100%;}
+    } 
+    </style>
 <body>
 
     <div class="container">
